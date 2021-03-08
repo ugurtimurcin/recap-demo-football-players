df6b98e90a0fe8fd3c8297c9d146273399964058
Ugur Timurcin <utimurcin@outlook.com>
2021-03-08 15:16:52 +0300

    AutofacBusinessModule created. (It will used instead of Microsoft DI)

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ClubManager>().As<IClubService>().SingleInstance();
+            builder.RegisterType<EfClubDal>().As<IClubDal>().SingleInstance();
+
+            builder.RegisterType<PlayerManager>().As<IPlayerService>().SingleInstance();
+            builder.RegisterType<EfPlayerDal>().As<IPlayerDal>().SingleInstance();
+
+            builder.RegisterType<PositionManager>().As<IPositionService>().SingleInstance();
+            builder.RegisterType<EfPositionDal>().As<IPositionDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+           
+        }
+    }
+}
