356b0632b26cd557e95fac63abde32c9ef7ec26a
Ugur Timurcin <utimurcin@outlook.com>
2021-03-08 15:20:06 +0300

    Context created

---
+++ DataAccess/Concrete/EntityFramework/Context/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Concrete.EntityFramework.Mapping;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SQLEXPRESS; Database = FootballPlayerDb; Integrated Security = True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ClubMap());
+            modelBuilder.ApplyConfiguration(new PositionMap());
+            modelBuilder.ApplyConfiguration(new PlayerMap());
+        }
+
+
+        public DbSet<Club> Clubs{ get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
