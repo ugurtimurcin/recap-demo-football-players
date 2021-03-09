ec3bb1e2fd8047cbed490d6eed627ee416c4b01a
Ugur Timurcin <utimurcin@outlook.com>
2021-03-09 19:51:27 +0300

    Position component imported

--- RecapProject.FootballPlayers/ClientApp/src/app/app.module.ts
+++ RecapProject.FootballPlayers/ClientApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { PositionsModule } from './positions/positions.module';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import { ListComponent } from './positions/list/list.component'; 
 
 @NgModule({
   declarations: [
@@ -16,16 +20,18 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    PositionsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'positions', component: ListComponent },
     ])
   ],
   providers: [],
