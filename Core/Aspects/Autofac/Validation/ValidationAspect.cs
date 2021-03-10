78858965e2f522acd71b5265e8d835f74de80f6a
Ugur Timurcin <utimurcin@outlook.com>
2021-03-10 15:10:45 +0300

    ValidationAspect created.

---
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,37 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Validation
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private readonly Type _validationtype;
+        public ValidationAspect(Type validationType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validationType))
+            {
+                throw new System.Exception("This is not a validation class");
+            }
+
+            _validationtype = validationType;
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validationtype);
+            var entityType = _validationtype.BaseType.GetGenericArguments()[0];
+            var entities = invocation.Arguments.Where(x => x.GetType() == entityType);
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(validator, entity);
+            }
+        }
+    }
+}
